502c1df5ed97ac435734e57755f8cd9b53a35219
a.rahimzadeh <a.rahimzadeh@emofid.com>
2023-11-20 11:00:53 +0330

    Complete product context

--- src/main/java/product/ProductContext.java
+++ src/main/java/product/ProductContext.java
@@ -1,22 +1,43 @@
 package product;
 
+import calculator.StandardShippingCalculator;
 import calculator.WeightCalculator;
 
 public class ProductContext {
     private ProductState state;
     private WeightCalculator weightCalculator;
 
+    public ProductContext() {
+        this.state = new InTransitState();
+        this.weightCalculator = new StandardShippingCalculator();
+    }
 
     public ProductContext(ProductState state, WeightCalculator weightCalculator) {
         this.state = state;
         this.weightCalculator = weightCalculator;
     }
 
+    public void setState(ProductState state) {
+        this.state = state;
+    }
+
     public ProductState getState() {
         return state;
     }
 
-    public void setState(ProductState state) {
-        this.state = state;
+    public void setWeightCalculator(WeightCalculator weightCalculator) {
+        this.weightCalculator = weightCalculator;
+    }
+
+    public void printStatus() {
+        state.printStatus();
+    }
+
+    public double calculatePrice(double weight) {
+        return weightCalculator.calculatePrice(weight);
+    }
+
+    public void nextState() {
+        state.nextState(this);
     }
 }
--- src/test/java/product/ProductContextTest.java
+++ src/test/java/product/ProductContextTest.java
@@ -10,31 +10,31 @@ class ProductContextTest {
 
     @Test
     void initialStateShouldBeInTransit() {
-        // ProductContext productContext = new ProductContext();
-        // assertTrue(productContext.getState() instanceof InTransitState);
+        ProductContext productContext = new ProductContext();
+        assertTrue(productContext.getState() instanceof InTransitState);
     }
 
     @Test
     void nextStateShouldChangeStateToDelivered() {
-        // ProductContext productContext = new ProductContext();
-        // productContext.nextState();
-        // assertTrue(productContext.getState() instanceof DeliveredState);
+        ProductContext productContext = new ProductContext();
+        productContext.nextState();
+        assertTrue(productContext.getState() instanceof DeliveredState);
     }
 
     @Test
     void settingStandardShippingShouldCalculatePriceCorrectly() {
-        // ProductContext productContext = new ProductContext();
-        // productContext.setWeightCalculator(new StandardShippingCalculator());
-        // double price = productContext.calculatePrice(5.0);
-        // assertEquals(12.5, price, 0.01);
+        ProductContext productContext = new ProductContext();
+        productContext.setWeightCalculator(new StandardShippingCalculator());
+        double price = productContext.calculatePrice(5.0);
+        assertEquals(12.5, price, 0.01);
     }
 
     @Test
     void settingExpressShippingShouldCalculatePriceCorrectly() {
-        // ProductContext productContext = new ProductContext();
-        // productContext.setWeightCalculator(new ExpressShippingCalculator());
-        // double price = productContext.calculatePrice(5.0);
-        // assertEquals(17.5, price, 0.01);
+        ProductContext productContext = new ProductContext();
+        productContext.setWeightCalculator(new ExpressShippingCalculator());
+        double price = productContext.calculatePrice(5.0);
+        assertEquals(17.5, price, 0.01);
     }
 
 }
