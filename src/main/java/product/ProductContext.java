2f8780cc979967d4049f2348ce3e9e648e7865e9
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2023-11-20 10:10:14 +0330

    refactor: add constructor to ProductContext with getState

--- src/main/java/product/ProductContext.java
+++ src/main/java/product/ProductContext.java
@@ -6,4 +6,13 @@ public class ProductContext {
     private ProductState state;
     private WeightCalculator weightCalculator;
 
+
+    public ProductContext(ProductState state, WeightCalculator weightCalculator) {
+        this.state = state;
+        this.weightCalculator = weightCalculator;
+    }
+
+    public ProductState getState() {
+        return state;
+    }
 }
