887bc91250e55bf1a3f771349eb533925de48515
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2023-11-20 10:03:50 +0330

    refactor: implement bodies for tests in DeliveredStateTest.java

--- src/test/java/product/DeliveredStateTest.java
+++ src/test/java/product/DeliveredStateTest.java
@@ -2,15 +2,41 @@ package product;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeliveredStateTest {
 
     @Test
-    void printStatus() {
+    public void testPrintStatus() {
+        ProductState state = new DeliveredState();
+        String expectedStatus = "Package has been delivered.";
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        state.printStatus();
+
+        System.setOut(System.out);
+
+        assertEquals(expectedStatus, outputStream.toString().trim());
     }
 
     @Test
-    void nextState() {
+    public void testNextState() {
+        ProductState state = new DeliveredState();
+        ProductContext context = new ProductContext();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        state.nextState(context);
+
+        System.setOut(System.out);
+
+        String expectedOutput = "Package is already delivered. No further state change.";
+        assertEquals(expectedOutput, outputStream.toString().trim());
     }
 }
