5979ff145a44b497fa6f299346746b39198509bf
Shayan Shabanzadeh <shayan.shabanzadeh@peykasa.ir>
2023-11-20 09:30:39 +0330

    add: weigthCalculator and express and normal to calculator package with strategy design pattern. with out body

---
+++ src/main/java/calculator/ExpressShippingCalculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class ExpressShippingCalculator implements WeightCalculator {
+    @Override
+    public double calculatePrice(double weight) {
+        return 0;
+    }
+}
---
+++ src/main/java/calculator/StandardShippingCalculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class StandardShippingCalculator implements WeightCalculator {
+    @Override
+    public double calculatePrice(double weight) {
+        return 0;
+    }
+}
